903a4dce1006deecfa239992f455f0e65c3aa49c
John Evans <john@milo.com>
2011-04-14 15:34:22 -0700

    Made first test pass.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-class PortedTestsPart1(object):
+import unittest
+
+
+class Sheet(object):
+	def get(self, key):
+		return ""
+
+
+class PortedTestsPart1(unittest.TestCase):
 	def test_that_cells_are_empty_by_default(self):
 		sheet = Sheet()
 		self.assertEquals("", sheet.get("A1"))
 		self.assertEquals("", sheet.get("ZX347"))
+
+
+if __name__ == '__main__':
+	unittest.main()
